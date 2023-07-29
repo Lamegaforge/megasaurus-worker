becbe756b508dd34e6dd0043b56971a805d94b00
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-07-29 10:11:28 +0200

    Added DebugClipCommand

---
+++ app/Console/Commands/DebugClipCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Actions\FetchClipsFromExternalIds;
+use App\Models\Clip;
+
+class DebugClipCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:debug-clip-command {hook}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $hook = $this->argument('hook');
+
+        $clip = Clip::where('uuid', $hook)
+            ->orWhere('externalId', $hook)
+            ->firstOrFail();
+
+        $fetchedClips = app(FetchClipsFromExternalIds::class)->handle(
+            collect($clip->externalId),
+        );
+
+        dd($fetchedClips);
+    }
+}
